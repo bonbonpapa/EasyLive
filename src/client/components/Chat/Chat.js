afae122ebb276385d87e5d6754671575996de732
bonbonpapa <45809121+bonbonpapa@users.noreply.github.com>
2020-04-21 12:50:48 -0400

    config

--- src/client/components/Chat/Chat.js
+++ src/client/components/Chat/Chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import io from "socket.io-client";
-import { API_URL } from "./config.js";
+import { API_URL } from "../config.js";
 import Messages from "../Messages/Messages";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
