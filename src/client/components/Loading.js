3c288e264a1917c4df5e08afa30014b2606e521c
bonbonpapa <45809121+bonbonpapa@users.noreply.github.com>
2020-04-25 12:22:59 -0400

    loading page

--- src/client/components/Loading.js
+++ src/client/components/Loading.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 export default () => (
-  <div className="loading-wrapper">
-    <h4>Heroku is spinning up, one moment please...</h4>
-    <div className="loading">
-      <div className="background">
-        <div>Loading</div>
-      </div>
-    </div>
+  <div>
+    <h3>Heroku is spinning up, one moment please...</h3>
   </div>
 );
